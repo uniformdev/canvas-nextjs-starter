e7603a37fb42017fc0d7008a722320a91f79d14f
Alex Shyba <alexs@uniform.dev>
2021-12-14 22:52:05 -0800

    init

---
+++ pages/_app.js
@@ -0,0 +1,20 @@
+import { UniformTracker } from "@uniformdev/optimize-tracker-react";
+import { createDefaultTracker } from "@uniformdev/optimize-tracker-browser";
+import intentManifest from "../lib/intentManifest.json";
+
+const localTracker = createDefaultTracker({
+  intentManifest,
+});
+
+function MyApp({ Component, pageProps, scoring }) {
+  return (
+    <UniformTracker
+      trackerInstance={localTracker}
+      initialIntentScores={scoring}
+    >
+      <Component {...pageProps} />
+    </UniformTracker>
+  );
+}
+
+export default MyApp;
---
+++ pages/index.js
@@ -0,0 +1,47 @@
+import { CanvasClient } from "@uniformdev/canvas";
+import { Composition, Slot } from "@uniformdev/canvas-react";
+import { CANVAS_DRAFT_STATE } from "@uniformdev/canvas";
+
+function HeroComponent({ greeting }) {
+  return <h1>{greeting}</h1>;
+}
+
+const resolveRenderer = (component) => {
+  // choose the component based on the Canvas component type
+  // (you can also use a Map, switch, next/dynamic, etc here)
+  if (component.type === "hero") {
+    return HeroComponent;
+  }
+  return null;
+};
+
+export default function Home({ composition }) {
+  return (
+    <Composition data={composition} resolveRenderer={resolveRenderer}>
+      <Slot name="content" />
+    </Composition>
+  );
+}
+
+// read the value of preview from the Next.js context
+export async function getStaticProps({ preview }) {
+  // create the Canvas client
+  const client = new CanvasClient({
+    apiHost: process.env.UNIFORM_CLI_BASE_URL || "https://uniform.app",
+    apiKey: process.env.UNIFORM_API_KEY || "uf1hnh35q0xns6g0jukleme7z386mqqe4p2hayhkdj7usm7p77g6rd3576hn7m7g2kr545mau9alped9w5a39ezrgfslkkpet",
+    projectId: process.env.UNIFORM_PROJECT_ID || "cccb0f03-f9d7-421e-b395-4a70a1db0e7e",
+  });
+
+  // fetch the draft composition from Canvas by slug (you can also fetch by composition id)
+  const { composition } = await client.getCompositionBySlug({  
+    slug: "/",
+    state: CANVAS_DRAFT_STATE,
+  });
+
+  // set `composition` as a prop to the page
+  return {
+    props: {
+      composition,
+    },
+  };
+}
