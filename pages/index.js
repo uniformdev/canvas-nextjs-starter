d9d44576828c5e65507e3727b3cbcb00f44028a3
Alex Shyba <alex.shyba@uniform.dev>
2021-12-21 17:11:12 -0800

    updated readme and serialized files

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Composition, Slot } from "@uniformdev/canvas-react";
 import { CANVAS_DRAFT_STATE } from "@uniformdev/canvas";
 
 function HeroComponent({ greeting }) {
-  return <h1>Hero: {greeting}</h1>;
+  return <h2>Hero: {greeting}</h2>;
 }
 
 const resolveRenderer = (component) => {
@@ -19,10 +19,43 @@ const resolveRenderer = (component) => {
 export default function Home({ composition }) {
   return (
     <Composition data={composition} resolveRenderer={resolveRenderer}>
+      <h1>Uniform Starter for Next.js</h1>
+      <p>
+        Congratulations! You are now running Next.js app powered by Uniform.
+      </p>
+      <p>
+        Below you see two Hero components rendered from Uniform Canvas. The
+        placement of these components is controlled in Canvas and can be changed
+        without developer's involvement. The second Hero is configured to be
+        personalizable via Uniform Optimize. Try hitting the "Personalize"
+        button to see it personalize.
+      </p>
       <Slot name="content" />
       <Link href="?stackblitz=true">
         <button>Personalize</button>
       </Link>
+      <p>
+        See more about what else you can do in readme. Visit{" "}
+        <a
+          href="https://docs.uniform.app/canvas/tutorials/nextjs-tutorial"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          this Next.js tutorial
+        </a>{" "}
+        to learn more of what you can do with Uniform Canvas and Optimize.
+      </p>
+      <p>
+        Download{" "}
+        <a
+          href="https://chrome.google.com/webstore/detail/uniform-optimize/dcmlokofjljnfjcknpmhjocogllfbhkg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Uniform Optimize extension for Chrome
+        </a>{" "}
+        to interact with the current visitor context.
+      </p>
     </Composition>
   );
 }
@@ -31,9 +64,8 @@ export default function Home({ composition }) {
 export async function getStaticProps({ preview }) {
   // create the Canvas client
   const client = new CanvasClient({
-    apiHost: process.env.UNIFORM_CLI_BASE_URL || "https://uniform.app",
-    apiKey: "uf1hnh35q0xns6g0jukleme7z386mqqe4p2hayhkdj7usm7p77g6rd3576hn7m7g2kr545mau9alped9w5a39ezrgfslkkpet",
-    projectId: "cccb0f03-f9d7-421e-b395-4a70a1db0e7e",
+    apiKey: process.env.UNIFORM_API_KEY,
+    projectId: process.env.UNIFORM_PROJECT_ID,
   });
 
   // fetch the draft composition from Canvas by slug (you can also fetch by composition id)
